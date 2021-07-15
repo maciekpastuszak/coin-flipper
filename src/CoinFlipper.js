2ec69cc6558a01c458d8262eb95b6bbd93bcb81b
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-07-15 09:25:22 +0200

    add coin flipping logic

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Coin from './Coin.js'
+import {choice} from "./helpers.js"
 
 export default class CoinFlipper extends Component {
     static defaultProps = {
         coins: [
-            {side: 'orzel', imgSrc: "https://www.katalogmonet.pl/images/kaminski/1990_prz_120.gif"},
-            {side: 'reszka', imgSrc: "https://static.myvimu.com/photo/15/55371523_m.jpg"}
+            {side: 'orzel', imgSrc: "https://tinyurl.com/react-coin-heads-jpg"},
+            {side: 'reszka', imgSrc: "https://tinyurl.com/react-coin-tails-jpg"}
         ]
     };
     constructor(props) {
@@ -19,17 +20,30 @@ export default class CoinFlipper extends Component {
         this.handleClick = this.handleClick.bind(this);
     }
     flipCoin() {
-
+const newCoin = choice(this.props.coins);
+this.setState(st => {
+    let newState = {
+        ...st,
+        currCoin: newCoin,
+        nFlips: st.nFlips + 1
+    }
+    if(newCoin.side === 'orzel'){
+        newState.nOrzel += 1;
+    } else {
+        newState.nReszka += 1;
+    }
+    return newState;
+})
     }
     handleClick(e) {
-this.flipCoin();
+    this.flipCoin();
     }
     render() { 
         return (
             <div className="CoinFlipper">
                 <h2>Rzućmy monetą!</h2>
-                <p> Na {this.state.nFlips} rzutów, wyrzuciłeś {this.state.nOrzel} orłów i {this.state.nReszka} reszek</p>
                 <button onClick={this.handleClick}>Rzuć monetą!</button>
+                <p> Na {this.state.nFlips} rzutów, wyrzuciłeś {this.state.nOrzel} orłów i {this.state.nReszka} reszek</p>
             </div>
         )
     }
